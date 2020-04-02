e8d274339ce484c5a2041839533b93e3d4a7cb72
Kamil <kamil55525@gmail.com>
2020-04-03 00:18:20 +0200

    Add documentation and fix an algorithm

--- poisson_disc_sampling.py
+++ poisson_disc_sampling.py
@@ -1,26 +1,35 @@
 from itertools import product
 from typing import List
-import matplotlib.pyplot as plt
 import numpy as np
 
+Point = np.ndarray
+Shape = np.ndarray
+ListOfPoints = List[np.ndarray]
+NDimArray = np.ndarray
 
-def is_valid(sample: np.ndarray, cell_size: float, sample_region_size: np.ndarray, radius, grid: np.ndarray,
-             points: List[np.ndarray]):
+
+def _is_sample_valid(sample: Point,
+                     sample_region_size: Shape,
+                     grid: NDimArray,
+                     cell_size: float,
+                     radius: float,
+                     points: ListOfPoints) -> bool:
     if any(sample < 0) or any(sample > sample_region_size):
         return False
-    cell_index = (sample // cell_size).astype(int)  # np. [4,2,1,7]
-    start = np.fromiter((max(x_i, 0) for x_i in cell_index - 2), dtype=int)
-    grid_shape = grid.shape
-    print(grid_shape)
-    end = np.fromiter((min(x_i, length - 1) for x_i, length in zip(cell_index + 2, grid_shape)), dtype=int)
 
-    ranges = [list(range(start_, end_ + 1)) for (start_, end_) in zip(start, end)]
+    # sample's cell index in the grid
+    cell_index = (sample // cell_size).astype(int)
 
-    print(f'    Cell index = {cell_index}')
+    start = ((max(coordinate, 0)
+              for coordinate in cell_index - 2))
+    end = ((min(coordinate, length - 1)
+            for coordinate, length
+            in zip(cell_index + 2, grid.shape)))
+    ranges = [list(range(start_, end_ + 1))
+              for (start_, end_) in zip(start, end)]
+
     for index in product(*ranges):
-        point_index = grid[index] - 1
-        print(f'index = {index}')
-        print(f'point_index = {point_index}')
+        point_index = grid[index]
         if point_index != -1:
             distance = np.linalg.norm(sample - points[point_index])
             if distance < radius:
@@ -28,65 +37,79 @@ def is_valid(sample: np.ndarray, cell_size: float, sample_region_size: np.ndarra
     return True
 
 
-def poisson_disc_sampling(radius: float, sample_region_size: np.ndarray, sample_rejection_threshold: int):
-    sampling_space_dimension = sample_region_size.shape[0]
-    N = sampling_space_dimension
-    cell_size: float = radius / np.sqrt(N)
+def _get_random_n_dim_vector(dimension: int, min_length: float, max_length: float) -> Point:
+    # need n-1 angles, all in range [0; PI] except the last one which is in range [0; 2*PI]
+    random_angles = np.random.rand(dimension - 1) * np.pi
+    random_angles[-1] *= 2
+    length = np.random.uniform(min_length, max_length)
+    vector = np.empty(dimension)
+    for i in range(dimension):
+        x_i = length * np.product(np.sin(random_angles[:i]))
+        if i != dimension - 1:
+            x_i *= np.cos(random_angles[i])
+        vector[i] = x_i
 
-    grid_shape = np.ceil(sample_region_size / cell_size).astype(int)
+    return vector
+
+
+def poisson_disc_sampling(radius: float, sample_domain_size: Shape, sample_rejection_threshold=30) -> ListOfPoints:
+    """
+    Returns a list of random points from the sampling domain such that the distance between any two points
+    is at least the radius. The sampling is done using Bridson algorithm. This implementation supports sampling
+    in n-dimensional spaces.
+
+        Parameters
+        ----------
+        radius : float
+            The minimum distance between points.
+
+        sample_domain_size : ndarray
+            An array with dimensions of sampling domain. For example if [X, Y] array is provided,
+            the sampling space has 2 dimensions and is a X x Y rectangle. If [X, Y, Z] is provided the
+            sampling space is X x Y x Z cuboid. In general n-dimensional domains are supported.
+
+        sample_rejection_threshold : int, optional
+            The number which defines how much samples from the neighbourhood of a given point is tested.
+            Default is 30.
+
+        Returns
+        -------
+        points : List[ndarray]
+            A list of samples (points).
+
+        Notes
+        -----
+        If the sample_rejection_threshold parameter is too low, the points may not be distributed evenly.
+        There may be even some large free spaces. The default is set to be 30 - it value recommended
+        by the algorithm's author - Robert Bridson. More information in the paper linked below.
+        https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
+           """
+    dimension = sample_domain_size.shape[0]
+
+    cell_size: float = radius / np.sqrt(dimension)
+    grid_shape = np.ceil(sample_domain_size / cell_size).astype(int)
+
     # contains indexes of points in the 'points' list
-    grid: np.ndarray = np.full(shape=grid_shape, fill_value=0)
-    points: List[np.ndarray] = []
-    active_points: List[np.ndarray] = []
+    grid = np.full(shape=grid_shape, fill_value=-1)
+    points: ListOfPoints = []
+    active_points: ListOfPoints = []
 
-    initial_sample = np.random.rand(N)
+    initial_sample = np.random.rand(dimension) * sample_domain_size / 2
     active_points.append(initial_sample)
 
     while len(active_points) > 0:
-        sample_found = False
         random_index = np.random.choice(len(active_points))
         random_sample = active_points[random_index]
 
         for _ in range(sample_rejection_threshold):
-            sample_candidate = np.add(random_sample, __get_random_n_dim_vector(N, radius, 2 * radius))
-            if is_valid(sample_candidate, cell_size, sample_region_size, radius, grid, points):
+            sample_candidate = random_sample + _get_random_n_dim_vector(dimension, radius, 2 * radius)
+            if _is_sample_valid(sample_candidate, sample_domain_size, grid, cell_size, radius, points):
                 points.append(sample_candidate)
                 active_points.append(sample_candidate)
-                ind = [int(x) for x in (sample_candidate // cell_size)]
-                grid[ind] = len(points)
-                sample_found = True
+                candidate_grid_index = tuple(int(x) for x in (sample_candidate // cell_size))
+                grid[candidate_grid_index] = len(points) - 1
                 break
-        if not sample_found:
+        else:
             active_points.pop(random_index)
 
     return points
-
-
-def __get_random_n_dim_vector(dimension, min_length, max_length):
-    # need n-1 angles, all in range [0; PI] except the last one which is in range [0; 2*PI]
-    random_angles = [*(np.random.rand(dimension - 2) * np.pi), np.random.rand() * 2 * np.pi]
-    length = np.random.uniform(min_length, max_length)
-    direction = np.empty(dimension)
-    for i in range(dimension):
-        x_i = 1.0
-        for angle in random_angles[:i]:
-            x_i *= np.sin(angle)
-        if i != dimension - 1:
-            x_i *= np.cos(random_angles[i])
-        direction[i] = x_i
-
-    return direction * length
-
-
-def draw(points):
-    plt.plot(points, markersize=10, marker='*', mec='b')
-    plt.xlim(-5, 8)
-    plt.ylim(-5, 9)
-    plt.show()
-
-
-if __name__ == '__main__':
-    # print(__get_random_n_dim_vector(2, 2, 4))
-    # poisson_disc_sampling(1.0, np.array([4, 4]), 2)
-
-    draw(poisson_disc_sampling(1.0, np.array([4, 4]), 30))
